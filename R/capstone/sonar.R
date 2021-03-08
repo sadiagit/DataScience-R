b4e6f39e9db7b658ce3ab50fd0746add8710c146
sadia.boksh <sadia.boksh@gmail.com>
2021-03-08 22:50:57 +1100

    Capstone - CYO update

--- R/capstone/sonar.R
+++ R/capstone/sonar.R
@@ -1,154 +1,150 @@
-#install required packages
+
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 if(!require(mlbench)) install.packages("mlbench", repos = "http://cran.us.r-project.org")
+if(!require(matrixStats)) install.packages("matrixStats", repos = "http://cran.us.r-project.org")
+if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
 
-#load libraries
 library(tidyverse)
 library(caret)
 library(data.table)
 library(matrixStats)
 library(dplyr)
 library(mlbench)
-library(rmarkdown)
 
-#load Sonar data
 data("Sonar")
 
-#insect first 5 rows
+### Data Exploration
+
 head(Sonar)
 
-#data exploration
+
+### Plots and Principal Component Analysis
+
 dim(Sonar)
 
+# response variable - M and R
 unique(Sonar$Class)
+
+# any missing value?
 any(is.na(Sonar))
 
-mean(Sonar$Class == 'M')
+# proportion of M and R in data set
+df %>% group_by(class) %>%
+  summarise(n=n()) %>%
+  mutate( prop=n/sum(n))
 
-#converting the dataframe to a matrix for PCA
+
+
+### PCA ###
+
+#converting the data frame to a matrix for PCA
 x <- Sonar[, 1:60] %>% as.matrix()
 
 
 # scale the feature matrix
 x_centered <- sweep(x, 2, colMeans(x))
-scaled_X <- sweep(x_centered, 2, colSds(x), FUN = "/") 
+scaled_X <- sweep(x_centered, 2, colSds(x), FUN = "/")
 
 colMeans(scaled_X)
 colSds(scaled_X)
 
-# principal components and summary
+# principal components
 pca <- prcomp(scaled_X)
 
+#Summary of variable importance
 summary(pca)$importance[, 1:30]
 
-
-#Plot PC1 vs PC2
-data.frame(pca$x[,1:2], class=Sonar$Class) %>% 
+#Plot first 2 Pcs
+data.frame(pca$x[,1:2], class=Sonar$Class) %>%
   ggplot(aes(PC1,PC2, col = class))+
   geom_point() +
-  coord_fixed(ratio = 1)
+  coord_fixed(ratio = 1)+
+  stat_ellipse(type="norm", lwd = 1.5)
 
-#box plot first 10 PCS
 
-data.frame(pca$x[,1:10], class=Sonar$Class) %>% gather(PCs,Value, -class) %>%
+#Also plot for first 10 PCs:
+data.frame(pca$x[,1:10], class=Sonar$Class) %>%
+  gather(PCs,Value, -class) %>%
   ggplot(aes(PCs,Value, fill = class))+
   geom_boxplot()
 
-#Model fitting
+
+### Modeling
 set.seed(1, sample.kind = "Rounding")
 
-#partitioning to test and train set
+#80/20 data split to train and test set respectively
 test_index <- createDataPartition(Sonar$Class, times = 1, p = 0.2, list = FALSE)
 test_x <- scaled_X[test_index,]
 test_y <- factor(Sonar$Class[test_index])
 train_x <- scaled_X[-test_index,]
 train_y <- factor(Sonar$Class[-test_index])
 
-#logistic
+
+### Logistic Regression
 train_glm <- train(train_x, train_y,method = "glm")
 pred_glm <-  predict(train_glm,  test_x)
 acc_glm <- confusionMatrix(pred_glm,test_y)$overall['Accuracy']
+acc_glm
 
 
-# LDA
+### LDA
 set.seed(5, sample.kind = "Rounding")
 
 train_lda <- train(train_x, train_y, method = "lda")
 pred_lda <-  predict(train_lda,  test_x)
 acc_lda <- confusionMatrix(pred_lda,test_y)$overall['Accuracy']
+acc_lda
 
 
-#KNN
+### KNN
 set.seed(7, sample.kind = "Rounding")
 
 train_knn <- train(train_x, train_y, method = "knn", tuneGrid = data.frame(k=c(3:21)))
 pred_knn <-  predict(train_knn,  test_x)
 acc_knn <- confusionMatrix(pred_knn,test_y)$overall['Accuracy']
+
+acc_knn
+
+#best K
 train_knn$bestTune
 
+#plot accuracy vs k (# of neighbors)
+ggplot(train_knn, highlight = TRUE)
 
-#Random Forest
+
+### Random Forest
 set.seed(7, sample.kind = "Rounding")
 
 train_rf <- train(train_x, train_y, method = "rf", tuneGrid = data.frame(mtry=c(3, 5, 7, 9, 11, 13)), importance=TRUE)
 pred_rf <-  predict(train_rf,  test_x)
 acc_rf <- confusionMatrix(pred_rf,test_y)$overall['Accuracy']
 
+#best mtry value
 train_rf$bestTune
+
 varImp(train_rf)
 
+#plot accuracy vs random predictors
+ggplot(train_rf, highlight = TRUE)
 
-#ensemble
+
+### Ensemble
+
+#combine all above predictions
 c_preds<- cbind(glm = pred_glm, lda =pred_lda, knn = pred_knn, rf = pred_rf)
 
+# if more than 50% models predict M then predict 'M', otherwise predict 'R'
 ensem <- ifelse(rowMeans(c_preds == 1) > 0.5, 'M', 'R')
 
+## calc accuracy
 acc_ens <- mean(ensem == test_y)
 
-#model fitting on PCA transformed data
-sonar_pca <- pca$x[, 1:30]
+acc_ens
 
-#create test and train set
-test_index <- createDataPartition(Sonar$Class, times = 1, p = 0.2, list = FALSE)
-test_x_pca <- sonar_pca[test_index,]
-test_y_pca <- factor(Sonar$Class[test_index])
-train_x_pca <- sonar_pca[-test_index,]
-train_y_pca <- factor(Sonar$Class[-test_index])
+### Results
 
-set.seed(11, sample.kind = "Rounding")
-
-#logistic
-train_glm_pca <- train(train_x_pca, train_y_pca,method = "glm")
-pred_glm_pca <-  predict(train_glm_pca,  test_x_pca)
-acc_glm_pca <- confusionMatrix(pred_glm_pca,test_y_pca)$overall['Accuracy']
-
-set.seed(13, sample.kind = "Rounding")
-
-#lda
-train_lda_pca <- train(train_x_pca, train_y_pca,method = "lda")
-pred_lda_pca <-  predict(train_lda_pca,  test_x_pca)
-acc_lda_pca <- confusionMatrix(pred_lda_pca,test_y_pca)$overall['Accuracy']
-
-set.seed(15, sample.kind = "Rounding")
-
-#knn
-train_knn_pca <- train(train_x_pca, train_y_pca,method = "knn", tuneGrid = data.frame(k=c(3:21)))
-pred_knn_pca <-  predict(train_knn_pca,  test_x_pca)
-acc_knn_pca <- confusionMatrix(pred_knn_pca,test_y_pca)$overall['Accuracy']
-
-set.seed(17, sample.kind = "Rounding")
-
-#rf
-train_rf_pca <- train(train_x_pca, train_y_pca,method = "rf", tuneGrid = data.frame(mtry=c(3,5,7,9)), importance=TRUE)
-pred_rf_pca <-  predict(train_rf_pca,  test_x_pca)
-acc_rf_pca <- confusionMatrix(pred_rf_pca,test_y_pca)$overall['Accuracy']
-
-
-#model results -  raw data
-data.frame(glm=acc_glm, lda=acc_lda, knn=acc_knn, rf=acc_rf, ensem = acc_ens)
-
-#model results -  pca tranformed data
-data.frame(glm_pca=acc_glm_pca, lda_pca=acc_lda_pca, knn_pca=acc_knn_pca, rf_pca=acc_rf_pca)
+## Model Result (Raw data)
+data.frame("Method"= c('glm','lda','knn','rf','ensemble') ,"Accuracy" =c(acc_glm, acc_lda, acc_knn, acc_rf, acc_ens))
 
